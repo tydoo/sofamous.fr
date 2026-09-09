5958eef4f5b9ac0342526ead9b030122f980e510
BOYER Thomas <thomas.boyer2@interieur.gouv.fr>
2026-09-09 14:13:18 +0200

    Remove unused MailService dependency from HomeController and cleanup email-related code

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Service\MailService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -17,15 +16,7 @@ final class HomeController extends AbstractController {
     }
 
     #[Route('/home', name: 'home.home')]
-    public function home(MailService $mailService): Response {
-        $mailService->sendEmail(
-            to: 'thomas@tydoo.fr',
-            subject: 'Hello from Symfony',
-            body: $this->renderView('emails/simple.html.twig', [
-                'subject' => 'Hello from Symfony',
-                'content' => 'This is a test email sent from the HomeController.'
-            ])
-        );
+    public function home(): Response {
         return $this->render($this->isGranted('IS_AUTHENTICATED_FULLY') ? 'home/logged.html.twig' : 'home/not-logged.html.twig');
     }
 }
